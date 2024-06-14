8424b5587aeac8ce175057d94e8ea2da80faa3f5
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-06-14 22:19:20 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import Header from "@/components/Header";
+import Footer from "@/components/footer";
+
 export default function Home() {
   return (
   <>
   <Header/>
   <HeroSection/>
+  <Footer/>
   </>
   );
 }

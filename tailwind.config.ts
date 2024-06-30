f213e2d8d5081bab5fb28b3f62fcbcee362a409f
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-06-30 20:33:10 +0530

    fixed css issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config : Config = {
 
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     './src/*/.{js,jsx,ts,tsx}',
 	],
